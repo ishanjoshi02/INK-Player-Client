6641c478db19023aec713035ebb69a6303c15387
Ishan Joshi <ishanjoshi02@gmail.com>
2019-04-24 02:39:00 +0530

    added user details to view user

--- src/components/ViewUser/index.jsx
+++ src/components/ViewUser/index.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import TruffleContract from "truffle-contract";
 import { getWeb3 } from "../../utils/getWeb3.js";
-const Web3 = require("web3");
+// const Web3 = require("web3");
 const UserStoreArtifact = require("../../contracts/UserStore.json");
 const UserStore = TruffleContract(UserStoreArtifact);
+const VideoStoreArtifact = require("../../contracts/VideoStore.json");
+const VideoStore = TruffleContract(VideoStoreArtifact);
 
 class ViewUser extends Component {
   state = {
     name: "",
     username: "",
-    email: ""
+    email: "",
+    videoCount: 0
   };
   componentWillMount = async () => {
     const { email } = this.props.match.params;
@@ -27,9 +30,38 @@ class ViewUser extends Component {
           });
       });
     });
+    VideoStore.setProvider(web3.currentProvider);
+    VideoStore.at(`0x90154d3e6bcf0eb951b501eca479c1224fb125c6`).then(
+      vidInst => {
+        web3.eth.getAccounts().then(accInst => {
+          vidInst.getVideoListCount
+            .call({
+              from: accInst[0]
+            })
+            .then(
+              res => {
+                this.setState({ videoCount: res["words"][0] });
+              },
+              err => {
+                console.log(err);
+              }
+            );
+        });
+      }
+    );
   };
   render() {
-    return <div>User Profile</div>;
+    return (
+      <div>
+        <h1>User Profile</h1>
+        <br />
+        <h2>Name: {this.state.name}</h2>
+        <br />
+        <h3>Username: {this.state.username}</h3>
+        <br />
+        <h3>Video Count: {this.state.videoCount}</h3>
+      </div>
+    );
   }
 }
 
